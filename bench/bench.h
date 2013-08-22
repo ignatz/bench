95141f4d204b55ad21eb2016c2d7b7b1faf9735e
Sebastian Jeltsch <sjeltsch@kip.uni-heidelberg.de>
2013-08-23 01:52:32 +0200

    fix rounding issue and add flexible layouts

--- bench/bench.h
+++ bench/bench.h
@@ -10,26 +10,22 @@
 #include <iomanip>
 #include <map>
 
-#define BENCH_THRESH    std::chrono::seconds(1)
-#define BENCH_DURATION  std::chrono::nanoseconds
-#define BENCH_CLOCK     std::chrono::steady_clock
-#define BENCH_SPACING   12
-
-#define BENCH_QUOTE(STR) #STR
-#define BENCH_TO_STRING(STR) BENCH_QUOTE(STR)
-
 namespace bench {
 
 using namespace std::chrono;
 
-typedef time_point<BENCH_CLOCK> time_t;
+// some defines
+typedef steady_clock      clock;
+typedef time_point<clock> time_t;
+typedef nanoseconds       duration;
 
-namespace detail {
+// iterate test until `thresh` time has passed
+static seconds const thresh = seconds(1);
 
 inline
-double reference_time(
+double find_or_store_reference(
 	std::string const& name,
-	double const measured_frequency)
+	double const frequency)
 {
 	typedef std::map<std::string, double> type;
 	static type _m;
@@ -38,45 +34,78 @@ double reference_time(
 	if ((it = _m.find(name)) != _m.end())
 		return it->second;
 
-	_m[name] = measured_frequency;
-	return measured_frequency;
+	_m[name] = frequency;
+	return frequency;
 }
 
+namespace detail {
+
 inline
-double duration_to_frequency(
+double to_frequency(
 	size_t const iterations,
-	BENCH_DURATION const duration)
+	duration const duration)
 {
 	return double(iterations) / duration_cast<nanoseconds>(duration).count() *
 		nanoseconds::period::den;
 }
 
-template<typename Time>
 inline
-BENCH_DURATION delta_t(Time const t)
+duration delta_t(time_t const& t)
 {
-	return duration_cast<BENCH_DURATION>(BENCH_CLOCK::now() - t);
+	return duration_cast<duration>(clock::now() - t);
 }
 
 inline
-void message(
-	std::string const& name,
-	size_t const iterations,
-	BENCH_DURATION const duration)
+size_t iterations(duration const& d)
 {
-	double frequency = duration_to_frequency(iterations, duration);
-	double reference = reference_time(name, frequency);
-
-	using namespace std;
-	cout << setw(BENCH_SPACING) << left << name << right;
-	cout.precision(2);
-	cout << setw(BENCH_SPACING) << scientific << frequency   << " it/s    "
-	     << setw(BENCH_SPACING) << scientific << 1/frequency << " s/it    "
-	     << setw(BENCH_SPACING) << fixed << reference/frequency * 100 << "%"
-		 << std::endl;
+	size_t iter = thresh / (d.count() ? d : nanoseconds(1));
+	return iter ? iter : 1;
 }
 
 } // namespace detail
+
+struct DefaultLayout
+{
+	DefaultLayout(std::ostream& os = std::cout) : _os(os) {}
+
+	void operator() (std::string const& name,
+					 double const frequency) const
+	{
+		static size_t const spacing = 12;
+
+		double const reference = find_or_store_reference(name, frequency);
+
+		_os << std::setw(spacing) << std::left << name << std::right;
+		_os.precision(2);
+		_os << std::setw(spacing) << std::scientific << frequency   << " it/s    "
+		    << std::setw(spacing) << std::scientific << 1/frequency << " s/it    "
+		    << std::setw(spacing) << std::fixed << reference/frequency * 100 << "%"
+		    << std::endl;
+	}
+
+private:
+	std::ostream& _os;
+};
+
+struct CSVLayout
+{
+	CSVLayout(std::ostream& os = std::cout) : _os(os) {}
+
+	void operator() (std::string const& name,
+					 double const frequency) const
+	{
+		double const reference = find_or_store_reference(name, frequency);
+
+		_os << "# name, it/s, s/it, %" << std::endl;
+		_os << name << ", "
+		    << std::scientific << frequency   << ", "
+		    << std::scientific << 1/frequency << ", "
+		    << std::fixed << reference/frequency * 100 << std::endl;
+	}
+
+private:
+	std::ostream& _os;
+};
 
 template <class T>
 inline
@@ -85,38 +114,42 @@ void preserve(T&& val)
 	  asm volatile("" : "+r" (val));
 }
 
-template<typename Lambda>
+template<typename Lambda, typename Layout>
 void run(
 	std::string const& name,
 	Lambda const& code,
+	Layout const& layout = DefaultLayout(),
 	size_t const iterations = 1,
 	size_t const offset = 0,
-	time_t const time = BENCH_CLOCK::now())
+	time_t const time = clock::now())
 {
 	for (size_t ii = offset; ii < iterations ; ++ii)
 		code();
 
-	detail::message(name, iterations, detail::delta_t(time));
+	double frequency = detail::to_frequency(iterations, detail::delta_t(time));
+	layout(name, frequency);
 }
 
 } // namespace bench
 
-#define BENCH(NAME, ...) \
+#define BENCH_L(NAME, LAYOUT, ...) \
 { \
 	auto __lambda = [&](){ __VA_ARGS__ }; \
-	auto __t = BENCH_CLOCK::now(); \
+	auto __t = bench::clock::now(); \
 	__lambda(); \
 	auto __d = bench::detail::delta_t(__t); \
-	size_t __end = BENCH_THRESH / \
-		(__d.count() ? __d : std::chrono::nanoseconds(1)); \
-	bench::run(BENCH_TO_STRING(NAME), __lambda, __end, 1, __t); \
+	size_t __i = bench::detail::iterations(__d); \
+	bench::run( #NAME , __lambda, LAYOUT, __i, 1, __t); \
+}
+
+#define BENCH(NAME, ...) \
+	BENCH_L(NAME, bench::DefaultLayout(), __VA_ARGS__)
+
+#define BENCH_NL(NAME, N, LAYOUT, ...) \
+{ \
+	auto __lambda = [&](){ __VA_ARGS__ }; \
+	bench::run( #NAME , __lambda, LAYOUT, N); \
 }
 
 #define BENCH_N(NAME, N, ...) \
-{ \
-	auto __lambda = [&](){ __VA_ARGS__ }; \
-	bench::run(BENCH_TO_STRING(NAME), __lambda, N); \
-}
-
-#undef BENCH_DURATION
-#undef BENCH_SPACING
+	BENCH_NL(NAME, N, bench::DefaultLayout(), __VA_ARGS__)
--- test/main.cc
+++ test/main.cc
@@ -1,5 +1,7 @@
-#include "bench/bench.h"
+#include <bench/bench.h>
 #include <cmath>
+#include <chrono>
+#include <thread>
 
 int main(int, char const**)
 {
@@ -25,6 +27,23 @@ int main(int, char const**)
 		bench::preserve(x);
 		x += sqrt(23);
 	)
+
+	BENCH(TestBenchName,
+		std::chrono::seconds d(5);
+		std::this_thread::sleep_for(d);
+	)
+
+	BENCH_N(TestBenchName, 1,
+		std::chrono::seconds d(1);
+		std::this_thread::sleep_for(d);
+	)
+
+	bench::run("SomeOtherName",
+			   []() {
+					std::chrono::milliseconds d(1337);
+					std::this_thread::sleep_for(d);
+			   },
+			   bench::CSVLayout());
 
 	return 0;
 }
